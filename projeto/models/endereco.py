be371f71e1d6ec31d54fed3055fd3e18345077d7
pawerguido <134095128+fabriciobomfin@users.noreply.github.com>
2024-10-10 11:55:44 +0000

    update

--- projeto/models/endereco.py
+++ projeto/models/endereco.py
@@ -2,7 +2,7 @@ from models.enums.unidade_federativa import UnidadeFederativa
 
 
 class Endereco:
-    def __init__(self, logradouro: str, numero: int, complemento: str, cep: int, cidade: str, uf: UnidadeFederativa) -> None:
+    def __init__(self, logradouro: str, numero: int, complemento: str, cep: str, cidade: str, uf: UnidadeFederativa) -> None:
         self.logradouro = logradouro
         self.numero = self._verificar_tipo_numero_da_casa(numero)
         self.complemento = complemento
--- projeto/models/enums/unidade_federativa.py
+++ projeto/models/enums/unidade_federativa.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 class UnidadeFederativa(Enum):
     BAHIA = ("Bahia", "BA")
-    SÃO_PAULO = ("São Paulo", "SP")
+    SAO_PAULO = ("São Paulo", "SP")
     RIO_DE_JANEIRO = ("Rio de Janeiro", "RJ")
 
     def __init__(self, nome, sigla):
